7526cde010eedbbc1e3b5d20e85af850df209a96
marin0suke <monk.marina@gmail.com>
2024-11-05 11:48:43 +1000

    Add HTML button and create setupEventListeners bones

---
+++ src/setupEventListeners.js
@@ -0,0 +1,15 @@
+// add event listeners here. start by adding add project button and add todo button.
+
+
+function setupEventListeners() {
+    document.querySelector(".create-todo").addEventListener("click", () => {
+        // want form to pop up - need to create the form in a diff component and call here.
+        // 1. call the form function.
+        // save the input.values into a todo obj.
+        // add the obj into todo array? - 
+    });
+
+    document.querySelector(".create-project").addEventListener("click", () => {
+
+    });
+}
