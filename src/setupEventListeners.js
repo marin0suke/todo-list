a582c7d7265fe58ee2f88033c648e0f03c3074e4
marin0suke <monk.marina@gmail.com>
2024-11-05 13:02:50 +1000

    Add function to show and hide form, and submit form values then reflect in projects display.

--- src/setupEventListeners.js
+++ src/setupEventListeners.js
@@ -1,15 +1,38 @@
 // add event listeners here. start by adding add project button and add todo button.
 
+import AppController from "./appController";
+import { renderProjects, toggleTodoForm } from "./domRenderer";
+
 
 function setupEventListeners() {
     document.querySelector(".create-todo").addEventListener("click", () => {
-        // want form to pop up - need to create the form in a diff component and call here.
-        // 1. call the form function.
-        // save the input.values into a todo obj.
-        // add the obj into todo array? - 
+        toggleTodoForm(true);
     });
+
+    document.querySelector(".todo-form").addEventListener("submit", (event) => {
+        // clicking button on form same as submit event for form.
+        event.preventDefault; // not sure if need yet.
+        // on submit, the input attributes are usable. 
+
+        const title = event.target.title.value;
+        const description = event.target.description.value;
+        const priority = event.target.description.value;
+        const dueDate = event.target.description.value;
+        // add completed here.
+        
+        const newTodo = createTodo(title, description, priority, dueDate, false);
+        AppController.addTodoToProject(newTodo, "All Todos");
+
+        toggleTodoForm(false); // hides form
+        event.target.reset();
+
+        renderProjects(AppController.getAllProjects()); // rerenders with the added info.
+
+    })
 
     document.querySelector(".create-project").addEventListener("click", () => {
 
     });
 }
+
+export { setupEventListeners };
